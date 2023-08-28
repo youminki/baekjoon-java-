b16519eaa36fc89b070180dec0627a2a903bd5d2
youminki <108206432+youminki@users.noreply.github.com>
2023-08-28 19:29:23 +0900

    Create 고양이.java

---
+++ 기초/고양이.java
@@ -0,0 +1,9 @@
+// 고양이 10171번
+// https://www.acmicpc.net/problem/10171
+package 기초;
+
+public class 고양이 {
+    public static void main(String[] args){
+        System.out.println("\\    /\\\n )  ( \')\n(  /  )\n \\(__)|");
+    }
+}
