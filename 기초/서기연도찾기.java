7caab4ca82ff24981d45710d4aaa968902e0f8c1
youminki <108206432+youminki@users.noreply.github.com>
2023-08-28 18:35:07 +0900

    commit

---
+++ 기초/서기연도찾기.java
@@ -0,0 +1,14 @@
+// 1998년생인 내가 태국에서는 2541년생?!
+// https://www.acmicpc.net/problem/18108
+package 기초;
+
+import java.util.Scanner;
+
+public class 서기연도찾기 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int BC = sc.nextInt();
+        int AD =  BC - 543;
+        System.out.print(AD);
+    }
+}
