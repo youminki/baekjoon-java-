13013d764676e1d222119acc4784ee65d7b71aef
youminki <108206432+youminki@users.noreply.github.com>
2023-08-28 19:18:23 +0900

    commit

---
+++ 기초/꼬마정민.java
@@ -0,0 +1,16 @@
+// 꼬마정민.java 11382번
+// https://www.acmicpc.net/problem/11382
+package 기초;
+
+import java.util.Scanner;
+
+public class 꼬마정민 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        long A = sc.nextLong();
+        long B = sc.nextLong();
+        long C = sc.nextLong();
+        long result = A + B + C;
+        System.out.println(result);
+    }
+}
