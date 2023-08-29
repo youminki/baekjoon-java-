39eb197f2b20274788322647e00920a290525e7b
youminki <108206432+youminki@users.noreply.github.com>
2023-08-30 05:14:43 +0900

    Create 개수세기.java

---
+++ 일차원배열/개수세기.java
@@ -0,0 +1,21 @@
+// 개수세기.java 10807
+// https://www.acmicpc.net/problem/10807
+package 일차원배열;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class 개수세기 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        for(int i=0; i <  N; i++){
+            int v = Integer.parseInt(br.readLine());
+        }
+        if(v ==)
+    }
+}
