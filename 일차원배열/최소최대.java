c0f874a8e63e12ac9d5ec9ce22493d321581f48a
youminki <108206432+youminki@users.noreply.github.com>
2023-08-31 16:38:35 +0900

    Update 최소최대.java

--- 일차원배열/최소최대.java
+++ 일차원배열/최소최대.java
@@ -1,7 +1,6 @@
 // https://www.acmicpc.net/problem/10818
 //최소최대.java 10818
 package 일차원배열;
-
 import java.util.Scanner;
 
 public class 최소최대 {
@@ -9,8 +8,8 @@ public class 최소최대 {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int arr[] = new int[N];
-        int max = -1000001;
-        int min = 1000001;
+        int max = arr[0];
+        int min = arr[0];
         for(int i=0; i < N; i++){
             arr[i] = sc.nextInt();
         }
@@ -22,6 +21,40 @@ public class 최소최대 {
                 min = arr[i];
             }
         }
-        System.out.printf("%d %d", min, max);
+        System.out.print(min + " " + max);
     }
 }
+
+
+// import java.io.BufferedReader;
+// import java.io.BufferedWriter;
+// import java.io.IOException;
+// import java.io.InputStreamReader;
+// import java.io.OutputStreamWriter;
+// import java.util.StringTokenizer;
+
+// public class 최소최대 {
+//     public static void main(String[] args) throws IOException{
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//         int N = Integer.parseInt(br.readLine());
+//         int arr[] = new int[N];
+//         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+//         for(int i=0; i < N; i++){
+//             arr[i] = Integer.parseInt(st.nextToken());
+//         }
+        
+//         int max = arr[0];
+//         int min = arr[0];
+//         for(int i=0; i < N; i++){
+//             if(arr[i] < min){
+//                 min = arr[i];
+//             }
+//             if(arr[i] > max){
+//                 max = arr[i];
+//             }
+//         }
+//         bw.write(min + " " + max);
+//         bw.flush();
+//     }
+// }
