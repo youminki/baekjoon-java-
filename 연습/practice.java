a1dda880fa5a15ee95853faed8b6ef33b2274562
youminki <108206432+youminki@users.noreply.github.com>
2023-08-31 23:54:02 +0900

    commit

--- 연습/practice.java
+++ 연습/practice.java
@@ -8,40 +8,30 @@ import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class practice {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-
-        int arr[] = new int[10];
-        int temp = 0;
-        int count = 0;
-
-        // 10개의 숫자를 입력받고, 각 숫자를 42로 나눈 나머지를 배열에 저장
-        for (int i = 0; i < 10; i++) {
-            st = new StringTokenizer(br.readLine());
-            arr[i] = Integer.parseInt(st.nextToken()) % 42;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int temp;
+        int arr[] = new int[N];
+        for(int i=0; i < arr.length; i++){
+            arr[i] = i + 1;
         }
-
-        // 중복을 체크하고 중복되지 않은 나머지 값의 개수를 세는 부분
-        for (int i = 0; i < 10; i++) {
-            temp = 0; // temp 변수 초기화
-            for (int j = i + 1; j < 10; j++) { // 현재 위치(i) 다음부터 배열을 순회하며 중복 체크
-                if (arr[i] == arr[j]) { // 중복된 나머지 값을 찾으면 temp를 증가
-                    temp++;
-                }
-            }
-            if (temp == 0) { // 중복이 없으면 count를 증가
-                count++;
+        for(int i=0; i < M; i++){
+            int I = Integer.parseInt(st.nextToken()) - 1;
+            int J = Integer.parseInt(st.nextToken()) - 1;
+            temp = 0;
+            while(true){
+                temp = arr[I];
+                arr[I++] = arr[J];
+                arr[J--] = temp;
             }
         }
-
-        // 중복되지 않은 나머지 값의 개수를 출력
-        bw.write(String.valueOf(count));
-        bw.newLine();
+        for(int i=0; i < arr.length; i++){
+            bw.write(arr[i] + " ");
+        }
         bw.flush();
-        
-        br.close();
-        bw.close();
     }
 }
--- 일차원배열/바구니뒤집기.java
+++ 일차원배열/바구니뒤집기.java
@@ -2,22 +2,30 @@
 // 바구니뒤집기.java 10811
 package 일차원배열;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class 바구니뒤집기 {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-        int arr[] = new int[N];
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int arr[] = new int [N];
         int temp;
+
         for(int i=0; i < arr.length; i++){
             arr[i] = i + 1;
-            //System.out.println(i);
         }
         for(int i=0; i < M; i++){
-            int I = sc.nextInt() - 1;
-            int J = sc.nextInt() - 1;
+            st = new StringTokenizer(br.readLine());
+            int I = Integer.parseInt(st.nextToken()) - 1;
+            int J = Integer.parseInt(st.nextToken()) - 1;
             while(I < J){
                 temp = arr[I];
                 arr[I++] = arr[J];
@@ -25,7 +33,35 @@ public class 바구니뒤집기 {
             }
         }
         for(int i=0; i < arr.length; i++){
-            System.out.print(arr[i] + " ");
+            bw.write(arr[i] + " ");
         }
+        bw.flush();
     }
 }
+// import java.util.Scanner;
+
+// public class 바구니뒤집기 {
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int N = sc.nextInt();
+//         int M = sc.nextInt();
+//         int arr[] = new int[N];
+//         int temp;
+//         for(int i=0; i < arr.length; i++){
+//             arr[i] = i + 1;
+//             //System.out.println(i);
+//         }
+//         for(int i=0; i < M; i++){
+//             int I = sc.nextInt() - 1;
+//             int J = sc.nextInt() - 1;
+//             while(I < J){
+//                 temp = arr[I];
+//                 arr[I++] = arr[J];
+//                 arr[J--] = temp;
+//             }
+//         }
+//         for(int i=0; i < arr.length; i++){
+//             System.out.print(arr[i] + " ");
+//         }
+//     }
+// }
