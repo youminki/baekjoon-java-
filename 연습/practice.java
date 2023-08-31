e19a75b9e1563b0c921579f3f29954c37ed7bc5e
youminki <108206432+youminki@users.noreply.github.com>
2023-08-31 15:12:55 +0900

    commit

--- 연습/practice.java
+++ 연습/practice.java
@@ -1,6 +1,4 @@
-// 개수세기.java 10807
-// https://www.acmicpc.net/problem/10807
-package 일차원배열;
+package 연습;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -9,20 +7,20 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-public class 개수세기 {
+public class practice {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedWriter bw =  new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
+        int [] arr = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0; i <  arr.length; i++){
+        for(int i=0; i < arr.length; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        int searchNum = Integer.parseInt(br.readLine());
+        int search = Integer.parseInt(br.readLine());
         int count = 0;
         for(int i=0; i < arr.length; i++){
-            if(arr[i] == searchNum){
+            if(arr[i] == search){
                 count++;
             }
         }
