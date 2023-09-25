c08cfe576c870335556821eeef9ababc4f14ea5b
youminki <108206432+youminki@users.noreply.github.com>
2023-09-25 13:49:29 +0900

    Commit

---
+++ 이충기/오장24번.java
@@ -0,0 +1,68 @@
+// 빠른 알고리즘
+
+package 이충기;
+
+public class 오장24번 {
+    // 배열 S[low .. high]를 S[low]를 기준으로 하여 분할한다.
+    // S[low]보다 큰 요소들을 S[low]의 오른쪽에 배치하고
+    // 나머지 요소들을 S[low]의 왼쪽에 배치한다.
+    public static int partition(int S[], int low, int high){
+        int i, j, temp;
+        i = low + 1;
+        j = high;
+
+        // 배열을 S[low]를 기준으로 하여
+        // S[low]보다 큰 요소는 배열의 오른쪽으로 옮기고
+        // 작은 요소는 배열의 왼쪽으로 옮긴다.
+        while(i <= j){
+            if(S[i] <= S[low]){
+                i += 1;
+            }
+            else if(S[j] > S[low]){
+                j -= 1;
+            }
+            else{
+                temp = S[i];
+                S[i] = S[j];
+                S[j] = temp;
+                i += 1;
+                j -= 1;
+            }
+        }
+
+        // S[low]를 S[low]보다 작거나 같은 요소들의 구역의 마지막 요소와 교환한다.
+        temp = S[low];
+        S[low] = S[j];
+        S[j] = temp;
+        return j;
+    }
+
+    //배열 S[low .. high]를 정렬한다.
+    public static void quickSort(int S[], int low, int high){
+        int pivoPoint;
+        if(low < high){
+            // 배열 S[low .. high]를 분할할 지수를 계산한다.@interface
+            pivoPoint = partition(S, low, high);
+            // S[low .. pivoPoint - 1]을 빠른 정렬을 사용하여 정렬한다.
+            quickSort(S, low, pivoPoint - 1);
+            //S[pivoPoint + 1 .. high]을 빠른 정렬을 사용하여 정렬한다.@interface
+            quickSort(S, pivoPoint + 1, high);
+        }
+    }
+
+    //배열 A의 요소들을 출력한다
+    public static void printArray(int A[]){
+        for(int i =0; i < A.length; ++i){
+            System.out.print(A[i] + " ");
+        }
+    }
+     // 시작 메소드 main
+    public static void main(String args[]){
+        System.out.println("\n주어진 배열");
+        int A[] = {15, 22, 13, 27, 12, 10, 20, 25, 32};
+        printArray(A);
+        quickSort(A, 0, A.length - 1);
+        System.out.println("\n정렬된 배열");
+        printArray(A);
+    }
+}
---
+++ 이충기/오장41번.java
@@ -0,0 +1,61 @@
+// 선택 알고리즘
+package 이충기;
+//프로글매 : 배열 내에서 k번째로 작은 요소를 찾는다.
+public class 오장41번 {
+    //배열 S[first .. last]를 S[first]를 기준으로 하여 분할한다.
+    // S[first]보다 큰 요소들을 S[first]의 오른쪽에 배치하고 나머지
+    // 요소들을 S[first]의 왼족에 배칳나다.@interface
+    public static int partition(int S[], int first, int last){
+        int i, j, temp;
+        i = first + 1;
+        j = last;
+        // 배열의 S[first]를 기준으로 하여 S[first]보다 큰요소는 배열의
+        // 오른쪽으로 옮기고 작거나 같은 요소는 배열의 왼쪽으로 옮긴다.@interface
+        while(i <= j){
+            if(S[i] <= S[first]){
+                i += 1;
+            }
+            else if(S[j] > S[first]){
+                j -= 1;
+            }
+            else{
+                temp = S[i];
+                S[i] = S[j];
+                S[j] = temp;
+                i += 1;
+                j += 1;
+            }
+        }
+
+        //S[first]를 S[first]보다 작거나 같은 요소들의 구역의 마지막 요소와 교환한다.
+        temp = S[first];
+        S[first] = S[j];
+        S[j] = temp;
+        return j;
+    }
+
+    //배열 S[first .. last]내에서 k번째 작은 요소를 찾아서 반환한다.
+    public static int selection(int[] A, int first, int last, int k){
+        int p, S;
+        // 배열 A[first .. last]를 분할할 지수를 계산한다.
+        p = partition(A, first, last);
+        // A[p]보다 작거나 같은 요소들의 수를 계산한다.
+        S = (p - 1) - first + 1;
+        if(k <= S){
+            // A[p]보다 작거나 같은 요소들의 Small 그룹에서 찾는다.@interface
+            return selection(A, first, p - 1, k);
+        }
+        else if(k == S + 1){
+            return A[p]; // k번째 작은 요소를 찾았다.@interface
+        }
+        else{ //A[p]보다 큰 요소들을 Large 그룹에서 찾는다.
+            return selection(A, p + 1, last, k - S - 1);
+        }
+    }
+    // 시작 메소드 main
+    public static void main(String[] args){
+        int A[] = {48, 12, 70, 38, 75, 67, 96, 52, 81};
+        int k = 5;
+        System.out.println(k + "번째로 작은 요소 " + selection(A, 0, A.length - 1, k) );
+    }
+}
