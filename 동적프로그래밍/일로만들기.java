f48904c30aa012f9eec33b9c8f2b45c8bf9e1909
youminki <108206432+youminki@users.noreply.github.com>
2023-10-10 22:46:13 +0900

    Update 일로만들기.java

--- 동적프로그래밍/일로만들기.java
+++ 동적프로그래밍/일로만들기.java
@@ -10,7 +10,6 @@ public class 일로만들기 {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int[] dp = new int[N + 1];
-
         for(int i=2; i < N + 1; i++){
             dp[i] = dp[i - 1] + 1;
             if(i % 2 == 0){
